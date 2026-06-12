c1e524d19514aa42ce4d403ab0f94537c77a0d5b
zayen-hash <zayen-hash@users.noreply.github.com>
2026-06-12 15:56:16 +0600

    feat: add Microsoft Clarity tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,13 @@ export default function RootLayout({
             strategy="lazyOnload"
           />
         )}
+        <Script id="clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "x5tkk2esbf");`}
+        </Script>
       </head>
       <body className="flex min-h-screen flex-col">
         <ThemeProvider>
